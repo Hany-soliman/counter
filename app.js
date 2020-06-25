439bea941e8b5d47b0e04051289c7f77aac3c699
Hany Soliman <Hany.essam1993@gmail.com>
2020-06-25 05:18:24 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Set initial count
+let count = 0;
+
+// select value and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(btn => {
+    btn.addEventListener("click", (e)=>{
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+        count --;
+    } else if (styles.contains('increase')){
+        count ++;
+    } else {
+        count = 0;
+    }
+    // if(count > 0){
+    //     value.style.color = 'green';
+    // }
+    count === 0 ? value.style.color = 'black' : (count > 0 ? value.style.color = 'green' : value.style.color = 'red');
+    value.textContent = count;
+    });
+});
